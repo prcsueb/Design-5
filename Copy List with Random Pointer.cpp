b326d39ae1b728c7b828311dc53843c4687e9d41
prcsueb <premang.raithatha.1@gmail.com>
2024-10-03 14:12:52 -0700

    Completed Design-5

---
+++ Copy List with Random Pointer.cpp
@@ -0,0 +1,33 @@
+//time: O(n) space: O(1)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL) return NULL;
+        Node* ptr = head;
+        while(ptr != NULL) {
+            Node* copyNode = new Node(ptr -> val);
+            copyNode -> next = ptr -> next;
+            ptr -> next = copyNode;
+            ptr = ptr -> next -> next;
+        }
+        ptr = head;
+        while(ptr != NULL) {
+            if(ptr -> random != NULL) {
+                ptr -> next -> random = ptr -> random -> next;
+            }
+            ptr = ptr -> next -> next;
+        }
+        ptr = head;
+        Node* result = head -> next;
+        Node* copyPtr = result;
+        while(ptr != NULL) {
+            ptr -> next = copyPtr -> next;
+            ptr = ptr -> next;
+            if(ptr != NULL) {    
+                copyPtr -> next = ptr -> next;
+                copyPtr = copyPtr -> next;
+            }    
+        }
+        return result;
+    }
+};
